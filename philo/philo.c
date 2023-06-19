38465b84719b9b29faa490ad1d11af3abe2621f3
Hamza Oumansour <happtechno@gmail.com>
2023-06-19 16:48:11 +0100

    done

--- philo/philo.c
+++ philo/philo.c
@@ -6,16 +6,11 @@
 /*   By: houmanso <houmanso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 19:21:46 by houmanso          #+#    #+#             */
-/*   Updated: 2023/06/16 15:02:05 by houmanso         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:47:35 by houmanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-void	ff(void)
-{
-	system("leaks philo");
-}
 
 static int	destroy(t_info *info, t_philo *p, t_heap **heap)
 {
@@ -39,7 +34,7 @@ int	main(int ac, char *av[])
 	t_info	*info;
 	t_heap	*heap;
 	t_philo	*philo;
-	atexit(ff);
+
 	if (ac != 5 && ac != 6)
 		return (write(2, "no enaugh arrgament.", 21), 1);
 	heap = NULL;
